a1cfb9c9eca8fdc2c1c8ea85f6c473c5096189db
Hosain <cryptichasan925@gmail.com>
2025-08-01 16:30:23 +0600

    hero section updated and image optimized

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,15 +7,15 @@ export default function Header() {
    
       <header
         className="bg-cover bg-center  lg:bg-right bg-no-repeat pb-16 md:pb-26 mix-blend-lighten"
-        style={{ backgroundImage: "url('/hero-bg-original.png')" }}
+        style={{ backgroundImage: "url('/assets/hero/hero-bg-original-min.png')" }}
         >
         <main
           className="bg-contain bg-center bg-no-repeat"
-          style={{ backgroundImage: "url('/noise.png')" }}
+          style={{ backgroundImage: "url('/assets/hero/noise-min.png')" }}
         >
           <section
             className="bg-cover bg-[position:calc(100%+380px)_top] md:bg-[position:calc(100%+430px)_top] lg:bg-contain lg:bg-[position:calc(100%+400px)_top] bg-no-repeat"
-            style={{ backgroundImage: "url('/solar-system.svg')" }}
+            style={{ backgroundImage: "url('/assets/hero/solar-system.svg')" }}
           >
             <Navbar />
             <Hero />
