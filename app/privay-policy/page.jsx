930210ee6b221b6d06cc7fd3128f851e6dbb5db0
Hosain <cryptichasan925@gmail.com>
2025-08-01 20:01:26 +0600

    privacy policy updated

---
+++ app/privay-policy/page.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function TermsAndConditions() {
+  return (
+    <div>
+      <Navbar />
+      <div className="section">Hello world</div>
+
+      <Footer />
+    </div>
+  );
+}
